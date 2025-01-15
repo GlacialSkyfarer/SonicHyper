3f09d22cee3a4376c38fa16a0aedc65eacb7dfff
GlacialSkyfarer <curseflame1122@gmail.com>
2025-01-14 21:06:48 -0700

    Added peak title screen, rings, gun beetle explosions, ring effects

--- Actors/Enemies/Enemy.cs
+++ Actors/Enemies/Enemy.cs
@@ -6,6 +6,11 @@ public partial class Enemy : Actor
 
 	[Export] protected int scoreValue = 100;
 
+	[Export] protected PackedScene deathEffect;
+	[Export] protected double deathEffectDuration = 0.5;
+	[Export] protected AudioStream deathSound;
+	[Export] protected float deathSoundVolume = -12;
+
 	public override void _OnDeath(Node source)
     {
 
@@ -14,6 +19,37 @@ public partial class Enemy : Actor
 			((Sonic)source).currentScore += scoreValue;
 
 		}
+
+		AudioStreamPlayer sound = new();
+
+		GetParent().AddChild(sound);
+		sound.Stream = deathSound;
+		sound.VolumeDb = deathSoundVolume;
+		sound.Play();
+
+		Timer kill = new Timer();
+
+		sound.AddChild(kill);
+
+		kill.WaitTime = 1;
+
+		kill.Timeout += sound.QueueFree;
+		kill.Start();
+
+		Node3D deathEff = (Node3D)deathEffect.Instantiate();
+
+		GetParent().AddChild(deathEff);
+		deathEff.GlobalPosition = GlobalPosition;
+
+		Timer kill2 = new Timer();
+
+		deathEff.AddChild(kill2);
+
+		kill2.WaitTime = deathEffectDuration;
+
+		kill2.Timeout += deathEff.QueueFree;
+		kill2.Start();
+
 
         base._OnDeath(source);
     
--- Gizmos/Ring.cs
+++ Gizmos/Ring.cs
@@ -4,11 +4,53 @@ using System.Collections.ObjectModel;
 
 public partial class Ring : Area3D
 {
+
+	[Export] private AudioStream ringSound = null;
+
+	[Export] private float soundVolume = 0.0f;
 	
+	[Export] private PackedScene collectEffect;
+
+	[Export] private double collectEffectDuration = 0.5;
+
 	public void OnCollect(Node3D b) {
 
 		((Sonic)b).currentRings += 1;
 		((Sonic)b).currentScore += 10;
+
+		AudioStreamPlayer sound = new();
+
+		GetParent().AddChild(sound);
+		sound.Stream = ringSound;
+		sound.VolumeDb = soundVolume;
+		sound.Play();
+
+		Timer kill = new Timer();
+
+		sound.AddChild(kill);
+
+		kill.WaitTime = 1;
+
+		kill.Timeout += sound.QueueFree;
+		kill.Start();
+
+		GpuParticles3D effect = collectEffect.Instantiate<GpuParticles3D>();
+
+		effect.Emitting = true;
+
+		GetParent().AddChild(effect);
+
+		effect.GlobalPosition = GlobalPosition;
+
+		Timer kill2 = new Timer();
+
+		effect.AddChild(kill2);
+
+		kill2.WaitTime = collectEffectDuration;
+
+		kill2.Timeout += effect.QueueFree;
+		kill2.Start();
+
 		QueueFree();
 
 	}
